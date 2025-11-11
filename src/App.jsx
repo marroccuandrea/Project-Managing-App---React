5aa50401e8b59808865760075697fec210cc7cd1
marroccuandrea <marroccuandrea@yahoo.it>
2025-11-11 17:47:21 +0100

    [update] [add] css tailwind style to the Sidebar Component

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import ProjectSidebar from "./components/ProjectsSidebar";
 
 function App() {
   return (
-    <>
+    <main className="  h-screen my-8">
       <ProjectSidebar />
-    </>
+    </main>
   );
 }
 
--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -1,9 +1,13 @@
 export default function ProjectSidebar() {
   return (
-    <aside>
-      <h2>I tuoi progetti</h2>
+    <aside className=" md:w-1/3 bg-blue-500 px-8 py-16 rounded-r-lg text-stone-50 shadow-lg">
+      <h2 className=" mb-8 font-bold uppercase md:text-xl lg:text-2xl">
+        I tuoi progetti
+      </h2>
       <div>
-        <button>Aggiungi Progetto</button>
+        <button className=" bg-red-800 px-4 py-2 rounded-lg hover:bg-red-600 hover:text-gray-900 transition duration-250 shadow-md">
+          Aggiungi Progetto
+        </button>
       </div>
       <ul></ul>
     </aside>
