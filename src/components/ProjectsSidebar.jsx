58c295551b9f8d69e6a8f0cadd7c1e4d6d868e68
marroccuandrea <marroccuandrea@yahoo.it>
2025-11-11 17:14:17 +0100

    [add] Sidebar project selection component

---
+++ src/components/ProjectsSidebar.jsx
@@ -0,0 +1,11 @@
+export default function ProjectSidebar() {
+  return (
+    <aside>
+      <h2>I tuoi progetti</h2>
+      <div>
+        <button>Aggiungi Progetto</button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+}
