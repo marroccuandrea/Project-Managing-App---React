c4ba97ae2954818f940561bc42545fa619444122
marroccuandrea <marroccuandrea@yahoo.it>
2025-11-12 17:53:07 +0100

    [add] NewProject component with input fields

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,28 @@
+export default function NewProject() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <button>Annulla</button>
+        </li>
+        <li>
+          <button>Salva</button>
+        </li>
+      </ul>
+      <div>
+        <p>
+          <label>Titolo</label>
+          <input />
+        </p>
+        <p>
+          <label>Descrizione</label>
+          <textarea />
+        </p>
+        <p>
+          <label>Data di fine</label>
+          <input />
+        </p>
+      </div>
+    </div>
+  );
+}
